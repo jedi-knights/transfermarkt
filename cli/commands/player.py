0efdad484c803f5f6481b25050d3279a07a5184e
Omar Crosby <omar.crosby@gmail.com>
2022-12-01 23:35:00 -0500

    feat(player): Adding a cli command to dump all player data.

--- cli/commands/player.py
+++ cli/commands/player.py
@@ -1,3 +1,4 @@
+import os
 import click
 
 from transfermarket.market import Market
@@ -17,3 +18,45 @@ def list_players(team_id):
 
     for player in players:
         click.echo(player)
+
+
+@cli.command("dump")
+def dump_players():
+    """Dump all players."""
+    market = Market()
+
+    competition_count = 0
+    team_count = 0
+    player_count = 0
+    competitions = market.get_competitions()
+
+    if os.path.isfile("output.txt"):
+        os.remove("output.txt")
+
+    with open("output.txt", "w") as f:
+        for competition in competitions:
+            try:
+                competition_count += 1
+                click.echo(competition)
+                f.write(f"{competition}\n")
+
+                teams = market.get_teams(competition.id)
+                for team in teams:
+                    team_count += 1
+                    click.echo(f"\t{team}")
+                    f.write(f"\t{team}\n")
+
+                    players = market.get_players(team.id)
+                    for player in players:
+                        player_count += 1
+                        f.write(f"\t\t{player}\n")
+
+                    f.write("\n")
+
+                f.write("\n")
+            except ValueError as e:
+                click.echo(e)
+
+    click.echo(f"Competitions: {competition_count}")
+    click.echo(f"Teams: {team_count}")
+    click.echo(f"Players: {player_count}")
